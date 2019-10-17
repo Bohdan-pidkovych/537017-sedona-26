d7d9de0af903521ccc677d7a737ca1d0e118941f
Bohdan-pidkovych <52783471+Bohdan-pidkovych@users.noreply.github.com>
2019-10-17 17:53:37 +0300

    Добавил интерактивную карту

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ if (form != null) {
     var secondDate = form.querySelector("[name=date-2]");
     var adult = form.querySelector("[name=adult]");
     var children = form.querySelector("[name=children]");
+    var formInput = form.querySelectorAll("input");
     form.classList.add("appointment-hide");
     button.addEventListener("click", function (evt) {
         evt.preventDefault();
@@ -17,6 +18,19 @@ if (form != null) {
             form.classList.remove("appointment-animation-open");
             form.classList.add("appointment-animation-close");
             form.classList.add("appointment-hide");
+        }
+    });
+    form.addEventListener("submit", function (evt) {
+        if (!firstDate.value || !secondDate.value || !adult.value || !children.value) {
+            evt.preventDefault();
+            for (var i = 0; i < formInput.length; i++) {
+               formInput[i].classList.add("appointment-input-empty");
+            }
+        } else {
+            if (isStorageSupport) {
+                localStorage.setItem("adult", adult.value);
+                localStorage.setItem("children", children.value);
+            }
         }
     });
 }
