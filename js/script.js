c59929227b86cfc7141e2f956390ea15f2bb79b6
Bohdan-pidkovych <52783471+Bohdan-pidkovych@users.noreply.github.com>
2019-10-17 13:47:49 +0300

    Добавил анимацию

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 var button = document.querySelector(".appointment-button");
 var form = document.querySelector(".appointment-container");
-var firstDate = form.querySelector("[name=date-1]");
-button.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    form.classList.toggle("appointment-hide");
-    firstDate.focus();
-});
+if (form != null) {
+    var firstDate = form.querySelector("[name=date-1]");
+    var secondDate = form.querySelector("[name=date-2]");
+    var adult = form.querySelector("[name=adult]");
+    var children = form.querySelector("[name=children]");
+    form.classList.add("appointment-hide");
+    button.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        if (form.classList.contains("appointment-hide")) {
+            form.classList.remove("appointment-hide");           
+            form.classList.remove("appointment-animation-close");
+            form.classList.add("appointment-animation-open");
+            firstDate.focus();
+        } else {
+            form.classList.remove("appointment-animation-open");
+            form.classList.add("appointment-animation-close");
+            form.classList.add("appointment-hide");
+        }
+    });
+}
